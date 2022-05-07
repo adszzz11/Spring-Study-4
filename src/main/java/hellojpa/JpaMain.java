2e8bd050c668df2db878239f74115bcb019135e7
sm <adszzz11@naver.com>
2022-05-07 16:46:03 +0900

    step 2 / modified JpaMain

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,24 +4,52 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.transaction.Transactional;
+import java.util.List;
 
 public class JpaMain {
+
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
-        Member member=new Member();
-        member.setId(1L);
-        member.setName("hello");
+        try {
+            //create Member
+            Member member=new Member();
+            member.setId(3L);
+            member.setName("hello");
 
-        em.persist(member);
+            em.persist(member);
 
-        tx.commit();
+            //find Member
+//            Member member1 = em.find(Member.class, 1L);
+//            System.out.println("member1.getName() = " + member1.getName());
+//            System.out.println("member1.getId() = " + member1.getId());
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+//                    .setFirstResult(0)
+//                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member1 : result) {
+                System.out.println("member1.getName() = " + member1.getName());
+            }
 
 
-        em.close();
-        emf.close();
+            //modify Member
+//            member1.setName("thisisTest");
+
+            //remove Member
+//            em.remove(member1);
+
+            tx.commit();
+
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+            emf.close();
+        }
     }
 }
