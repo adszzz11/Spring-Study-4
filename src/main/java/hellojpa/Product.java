e8b814d8563caa09fa61f0537a76cacb6f88b02f
sm <adszzz11@naver.com>
2022-05-30 19:49:49 +0900

    step 10/ association mapping

--- src/main/java/hellojpa/Product.java
+++ src/main/java/hellojpa/Product.java
@@ -8,10 +8,12 @@ import java.util.List;
 public class Product {
 
     @Id @GeneratedValue
+    @Column(name = "PRODUCT_ID")
     private Long id;
 
     private String name;
 
-    @ManyToMany(mappedBy = "products")
-    private List<Member> members = new ArrayList<>();
+    @OneToMany(mappedBy = "product")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
+
 }
