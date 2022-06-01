e352e91b518fc9fdc4073e9153369458fed660ae
sm <adszzz11@naver.com>
2022-06-01 20:10:17 +0900

    step 10/ MappedSuperClass

--- src/main/java/hellojpa/BaseEntity.java
+++ src/main/java/hellojpa/BaseEntity.java
@@ -10,4 +10,36 @@ public abstract class BaseEntity {
     private String lastModifiedBy;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
 }
