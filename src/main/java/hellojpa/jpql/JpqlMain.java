8113cb55ade1255479275866a768ffb58dc95484
sm <adszzz11@naver.com>
2022-06-10 12:47:47 +0900

    step 12 / jpql - functions

--- src/main/java/hellojpa/jpql/JpqlMain.java
+++ src/main/java/hellojpa/jpql/JpqlMain.java
@@ -23,19 +23,33 @@ public class JpqlMain {
             em.persist(team);
 
 
-                Member member = new Member();
-                member.setUsername("관리자");
-                member.setTeam(team);
-                member.setMemberType(MemberType.ADMIN);
-                member.setAge(10);
+            Member member = new Member();
+            member.setUsername("관리자");
+            member.setTeam(team);
+            member.setMemberType(MemberType.ADMIN);
+            member.setAge(10);
 
-                em.persist(member);
+            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            member2.setTeam(team);
+            member2.setMemberType(MemberType.ADMIN);
+            member2.setAge(10);
 
-
+            em.persist(member2);
 
             em.flush();
             em.clear();
-            String query = "select nullif(m.username, '관리자') from Member m";
+//            String query = "select nullif(m.username, '관리자') from Member m";
+//            String query = "select concat('a', 'b') from Member m";
+//            String query = "select concat('a', 'b') from Member m";
+//            String query = "select substring(m.username, 2, 3) from Member m";
+//            String query = "select locate('de','abcdefg') from Member m";
+//            String query = "select size(t.members) from Team t";
+//            String query = "select index(t.members) from Team t";
+//            String query = "select function('group_concat', m.username) from Member m";
+            String query = "select group_concat(m.username) from Member m";
+
 
             List<String> resultList = em.createQuery(query, String.class).getResultList();
             for (String s : resultList) {
