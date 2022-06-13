9ad1c11223bbb1e09b123d25102067c8779a4b05
sm <adszzz11@naver.com>
2022-06-13 22:13:18 +0900

    step 12 / jpql - use Entity Direct

--- src/main/java/hellojpa/jpql/JpqlMain.java
+++ src/main/java/hellojpa/jpql/JpqlMain.java
@@ -1,7 +1,6 @@
 package hellojpa.jpql;
 
 
-
 import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -54,21 +53,16 @@ public class JpqlMain {
             em.flush();
             em.clear();
 
-//            String query = "select m from Member m join fetch m.team";
-//            String query = "select distinct t from Team t join fetch t.members";
-//            String query = "select distinct t from Team t join fetch t.members m"; // 원칙적으로 사용하면 안됨. fetch를 여러번 시도할 때만 할 것
-//            String query = "select t from Team t join t.members m"; // Collection Type 사용, select 추가로 나감. paging 사용 시 문제 생김
-//            String query = "select m from Member m join fetch m.team"; // 해결1. 서로 바꾸기
-            String query = "select t from Team t"; // 해결2. Lazy Loading, N+1 문제 발생
-            //해결 3. BatchSize
-            //해결 4. DTO
+//            String query = "select m from Member m where m = :member";
+//            String query = "select m from Member m where m.id = :memberId";
+            String query = "select m from Member m where m.team = :team";
 
 
-            List<Team> resultList = em.createQuery(query, Team.class).setFirstResult(0).setMaxResults(2).getResultList();
-
-            for (Team t : resultList) {
-                System.out.println("t = " + t.getName() + "," + t + ", members : " + t.getMembers().size() );
-            }
+            Member member1 = em.createQuery(query, Member.class)
+//                    .setParameter("member", member)
+                    .setParameter("team",team)
+                    .getSingleResult();
+            System.out.println("member1 = " + member1);
 
             tx.commit();
         } catch (Exception e) {
